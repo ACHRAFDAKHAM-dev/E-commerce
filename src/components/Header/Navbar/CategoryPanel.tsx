2ef514b7a04e91884e97f6a877dfe5de8c5cf413
Achraf DAkham <ashrafdakham@gmail.com>
2025-08-22 17:32:04 +0100

    feat: add CategoryPanel with nested submenus and drawer functionality

--- src/components/Header/Navbar/CategoryPanel.tsx
+++ src/components/Header/Navbar/CategoryPanel.tsx
@@ -1,65 +1,103 @@
-
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import { FaRegSquarePlus } from "react-icons/fa6";
+import { IoClose } from "react-icons/io5";
+import { Button } from '@mui/material';
+import "../Navbar/Style.css";
+import { Link } from 'react-router-dom';
 
+type Props = {
+  isOpenCategoryPanel: boolean;
+  setIsOpenCategoryPanel: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-function CategoryPanel(){
+function CategoryPanel(props: Props) {
 
-    const [open, setOpen] = React.useState(false);
+  const [subMenuIndex, setSubMenuIndex] = React.useState<number | null>(null);
 
-  const toggleDrawer = (newOpen: boolean) => () => {
-    setOpen(newOpen);
+  const openSubMenu = (index: number) => {
+    setSubMenuIndex(subMenuIndex === index ? null : index);
   };
 
-   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+  const DrawerList = (
+    <Box
+      className='Category-panel'
+      sx={{ width: 250 }}
+      role="presentation"
+    >
+      <h3 className='p-3 text-[16px] font-[500] flex items-center justify-between'>
+        Shop By Categories 
+        <IoClose 
+          className='cursor-pointer text-[20px] font-[500]'
+          onClick={() => props.setIsOpenCategoryPanel(false)}
+        />
+      </h3>
+     
+      <div className="scroll">
+        <ul className='w-full'>
+          <li className='list-none flex items-center relative'>
+            <Link to={'/'} className='w-full'>
+              <Button className='w-full !text-left !justify-start !px-3 !text-[rgba(0,0,0,0.8)]'>
+                Fashion
+              </Button>
+            </Link>
+            <FaRegSquarePlus
+              className='absolute top-[10px] right-[15px] cursor-pointer'
+              onClick={() => openSubMenu(0)}
+            />
+             
+            {subMenuIndex === 0 && (
+              <ul className='submenu absolute top-[100%] left-[0%] w-full pl-3'>
+                <li className='list-none relative'>
+                  <Link to={'/'} className='w-full'>
+                    <Button className='w-full !text-left !justify-start !px-3 !text-[rgba(0,0,0,0.8)]'>
+                      Apparel
+                    </Button>
+                  </Link>
+                  <FaRegSquarePlus
+                    className='absolute top-[10px] right-[15px] cursor-pointer'
+                  />  
+
+                  <ul className='inner-submenu absolute top-[100%] left-[0%] w-full pl-3'>
+                    <li className='list-none relative mb-1'>
+                      <Link to={'/'} className='link w-full !text-left !justify-start !px-3 transition text-[14px]'>
+                        Smart Tablet
+                      </Link> 
+                    </li>
+                    <li className='list-none relative mb-1'>
+                      <Link to={'/'} className='link w-full !text-left !justify-start !px-3 transition text-[14px]'>
+                        Crepe T-shirt
+                      </Link> 
+                    </li>
+                    <li className='list-none relative mb-1'>
+                      <Link to={'/'} className='link w-full !text-left !justify-start !px-3 transition text-[14px]'>
+                        Leather Watch
+                      </Link> 
+                    </li>
+                    <li className='list-none relative mb-1'>
+                      <Link to={'/'} className='link w-full !text-left !justify-start !px-3 transition text-[14px]'>
+                        Rolling Diamond
+                      </Link> 
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      </div>
     </Box>
   );
 
-    return (
- <div>
-      <Button onClick={toggleDrawer(true)}>Open drawer</Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
-      </Drawer>
-    </div>
-
-    );
-
+  return (
+    <Drawer
+      open={props.isOpenCategoryPanel}
+      onClose={() => props.setIsOpenCategoryPanel(false)} 
+    >
+      {DrawerList}
+    </Drawer>
+  );
 }
+
 export default CategoryPanel;
--- src/components/Header/Navbar/Navbar.tsx
+++ src/components/Header/Navbar/Navbar.tsx
@@ -1,177 +1,55 @@
-import { Button} from "@mui/material";
+import React, { useState } from 'react';
+import { Button } from "@mui/material";
 import { RiMenu2Fill } from "react-icons/ri";
 import { LiaAngleDownSolid } from "react-icons/lia";
 import { Link } from "react-router-dom";
 import { GoRocket } from "react-icons/go";
 import CategoryPanel from "./CategoryPanel";
 
+function Navbar() {
+  const [isOpenCategoryPanel, setIsOpenCategoryPanel] = useState(false);
 
-function Navbar (){
-    return (
-       <>
-        <nav className="py-2">
+  return (
+    <>
+      <nav className="py-2">
         <div className="container flex items-center justify-end gap-8">
-            <div className="col_1 w-[20%]">
-                <Button className="!text-black gap-2 w-full">
-                    <RiMenu2Fill className="text-[18px]" />
-                    Shop By Categories 
-                    <LiaAngleDownSolid className="text-[13px] ml-auto font-bold"/>
-                    </Button>
-            </div>
-            <div className="col_2 w-[60%]">
-                <ul className="flex items-center gap-2">
-                    <li className="list-none">
-                        <Link to="/" className="link transition text-[14px] font-[500]">
-                        <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                            Home
-                        </Button>
-                        </Link>
-                    </li>
-                    <li className="list-none">
-                        <Link to="/" className="link transition text-[14px] font-[500]">
-                            <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                                Fashion
-                            </Button>
-                        </Link>
-                    </li>
-                    <li className="list-none">
-                        <Link to="/" className="link transition text-[14px] font-[500]">
-                            <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                                Electronics
-                            </Button>
-                        </Link>
-                    </li>
-                    <li className="list-none">
-                        <Link to="/" className="link transition text-[14px] font-[500]">
-                            <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                                Bags
-                            </Button>
-                        </Link>
-                    </li>
-                    <li className="list-none">
-                        <Link to="/" className="link transition text-[14px] font-[500]">
-                            <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                                Footwear
-                            </Button>
-                        </Link>
-                    </li>
-                    <li className="list-none">
-                        <Link to="/" className="link transition text-[14px] font-[500]">
-                            <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                                Groceries
-                            </Button>
-                        </Link>
-                    </li>
-                    <li className="list-none">
-                        <Link to="/" className="link transition text-[14px] font-[500]">
-                            <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                                Beauty
-                            </Button>    
-                        </Link>
-                    </li>
-                    <li className="list-none">
-                        <Link to="/" className="link transition text-[14px] font-[500]">
-                            <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                                wellness
-                            </Button>   
-                        </Link>
-                    </li>
-                    <li className="list-none">
-                        <Link to="/" className="link transition text-[14px] font-[500]">
-                            <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                                Jewellery
-                            </Button>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="col_3 w-[20%] ">
-                <p className="text-[15px] font-[500] flex items-center gap-3 mb-0 mt-0">
-                    <GoRocket className="text-[18px]"/>
-                    Free International Delivery
-                </p>
-            </div>
+      
+          <div className="col_1 w-[20%]">
+            <Button
+              className="!text-black gap-2 w-full flex items-center justify-start"
+              onClick={() => setIsOpenCategoryPanel(true)} 
+            >
+              <RiMenu2Fill className="text-[18px]" />
+              Shop By Categories 
+              <LiaAngleDownSolid className="text-[13px] ml-auto font-bold" />
+            </Button>
+          </div>
+
+         
+          <div className="col_2 w-[60%]">
+            <ul className="flex items-center gap-2">
+             <li className="list-none"> <Link to="/" className="link transition text-[14px] font-[500]"> <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]"> Home </Button> </Link> </li> <li className="list-none"> <Link to="/" className="link transition text-[14px] font-[500]"> <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]"> Fashion </Button> </Link> </li> <li className="list-none"> <Link to="/" className="link transition text-[14px] font-[500]"> <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]"> Electronics </Button> </Link> </li> <li className="list-none"> <Link to="/" className="link transition text-[14px] font-[500]"> <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]"> Bags </Button> </Link> </li> <li className="list-none"> <Link to="/" className="link transition text-[14px] font-[500]"> <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]"> Footwear </Button> </Link> </li> <li className="list-none"> <Link to="/" className="link transition text-[14px] font-[500]"> <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]"> Groceries </Button> </Link> </li> <li className="list-none"> <Link to="/" className="link transition text-[14px] font-[500]"> <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]"> Beauty </Button> </Link> </li> <li className="list-none"> <Link to="/" className="link transition text-[14px] font-[500]"> <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]"> wellness </Button> </Link> </li> <li className="list-none"> <Link to="/" className="link transition text-[14px] font-[500]"> <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]"> Jewellery </Button> </Link> </li>
+            
+            </ul>
+          </div>
+
+         
+          <div className="col_3 w-[20%]">
+            <p className="text-[15px] font-[500] flex items-center gap-3 mb-0 mt-0">
+              <GoRocket className="text-[18px]" />
+              Free International Delivery
+            </p>
+          </div>
         </div>
-    </nav>
+      </nav>
 
-
-        {/* Category Panel Components */}
-    <CategoryPanel/>
-       </>
-    );
+      {/* Category Panel Components */}
+      <CategoryPanel
+        isOpenCategoryPanel={isOpenCategoryPanel}
+        setIsOpenCategoryPanel={setIsOpenCategoryPanel}
+      />
+    </>
+  );
 }
+
 export default Navbar;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Button} from "@mui/material";
-// import { RiMenu2Fill } from "react-icons/ri";
-// import { LiaAngleDownSolid } from "react-icons/lia";
-// import { Link } from "react-router-dom";
-// import { GoRocket } from "react-icons/go";
-
-
-// function Navbar (){
-// const listItems = [
-// {id:1,title:';klas;kl'},
-// {id:2,title:'lkl;l'},
-// {id:3,title:'klkl;kl'},
-// {id:4,title:';l;l;l'},
-// {id:5,title:';l;l;l'},
-// {id:6,title:';;l'},
-// {id:7,title:'kl;l'},
-// {id:8,title:';l;l'},
-// ]
-
-// const list = listItems.map((item)=>{
-// return (
-// <li className="list-none" key={item.id}>
-//        <Link to="/" className="link transition text-[14px] font-[500]">
-//             <Button className="link transition !text-black !font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-//                  {item.title}
-//              </Button>
-//         </Link>
-// </li>
-// )
-// })
-//     return (
-//         <nav className="py-2">
-//         <div className="container flex items-center justify-end gap-8">
-//             <div className="col_1 w-[20%]">
-//                 <Button className="!text-black gap-2 w-full">
-//                     <RiMenu2Fill className="text-[18px]" />
-//                     Shop By Categories 
-//                     <LiaAngleDownSolid className="text-[13px] ml-auto font-bold"/>
-//                     </Button>
-//             </div>
-//             <div className="col_2 w-[60%]">
-//                 <ul className="flex items-center gap-2">
-// 			{list}
-//                  </ul>
-//             </div>
-//             <div className="col_3 w-[20%] ">
-//                 <p className="text-[15px] font-[500] flex items-center gap-3 mb-0 mt-0">
-//                     <GoRocket className="text-[18px]"/>
-//                     Free International Delivery
-//                 </p>
-//             </div>
-//         </div>
-//     </nav>
-//     );
-// }
-// export default Navbar;
